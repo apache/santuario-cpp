5c4f6ec2d6dac9750210cb1bc8398630f536b927
Scott Cantor <scantor@apache.org>
2012-08-21 18:13:18 +0000

    Turn off broken default ID resolution by default.
    git-svn-id: https://svn.apache.org/repos/asf/santuario/xml-security-cpp/trunk@1375701 13f79535-47bb-0310-9956-ffa450edef68

--- xsec/framework/XSECEnv.cpp
+++ xsec/framework/XSECEnv.cpp
@@ -156,7 +156,7 @@ XSECEnv::XSECEnv(DOMDocument *doc) {
 												XMLFormatter::UnRep_CharRef));
 
 	// Set up IDs
-	m_idByAttributeNameFlag = true;		// At the moment this is on by default
+	m_idByAttributeNameFlag = false;		// At the moment this is on by default
 	// Register "Id" and "id" as valid Attribute names
 	registerIdAttributeName(s_Id);
 	registerIdAttributeName(s_id);

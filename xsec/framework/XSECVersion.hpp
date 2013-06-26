d5af1d53c8f82486a33046aa1aa26a80b26a28be
Scott Cantor <scantor@apache.org>
2013-06-26 01:00:32 +0000

    Bump version
    git-svn-id: https://svn.apache.org/repos/asf/santuario/xml-security-cpp/trunk@1496704 13f79535-47bb-0310-9956-ffa450edef68

--- xsec/framework/XSECVersion.hpp
+++ xsec/framework/XSECVersion.hpp
@@ -30,7 +30,7 @@
 
 #define XSEC_VERSION_MAJOR   1
 #define XSEC_VERSION_MEDIUM  7
-#define XSEC_VERSION_MINOR   1
+#define XSEC_VERSION_MINOR   2
 
 // --------------------------------------------------------------------------------
 //           Version Handling

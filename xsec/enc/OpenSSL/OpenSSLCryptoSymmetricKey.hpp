97e89b7744af0ab6bb6cf51bba8fec6bcdee98d0
Scott Cantor <scantor@apache.org>
2017-08-30 23:23:01 +0000

    Fix NULL warnings
    git-svn-id: https://svn.apache.org/repos/asf/santuario/xml-security-cpp/trunk@1806743 13f79535-47bb-0310-9956-ffa450edef68

--- xsec/enc/OpenSSL/OpenSSLCryptoSymmetricKey.hpp
+++ xsec/enc/OpenSSL/OpenSSLCryptoSymmetricKey.hpp
@@ -151,7 +151,7 @@ public :
                              SymmetricKeyMode mode = MODE_CBC,
                              const unsigned char * iv = NULL,
                              const unsigned char* tag = NULL,
-                             unsigned int taglen = NULL);
+                             unsigned int taglen = 0);
 
     /**
      * \brief Continue an decrypt operation using this key.

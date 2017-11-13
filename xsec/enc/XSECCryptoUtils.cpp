2c92b856fa1da3593804e1c5e227b65bc3be11b5
Scott Cantor <scantor@apache.org>
2017-11-13 14:46:51 +0000

    Fix regressions due to API changes.
    git-svn-id: https://svn.apache.org/repos/asf/santuario/xml-security-cpp/trunk@1815111 13f79535-47bb-0310-9956-ffa450edef68

--- xsec/enc/XSECCryptoUtils.cpp
+++ xsec/enc/XSECCryptoUtils.cpp
@@ -257,7 +257,7 @@ int XSEC_EXPORT CalculateXKMSAuthenticationKey(unsigned char * input, int inputL
 	Janitor<XSECCryptoKeyHMAC> j_k(k);
 	k->setKey(keyVal, 1);
 
-	XSECCryptoHash *h = XSECPlatformUtils::g_cryptoProvider->hashHMACSHA1();
+	XSECCryptoHash *h = XSECPlatformUtils::g_cryptoProvider->hashHMACSHA();
 	Janitor<XSECCryptoHash> j_h(h);
 	h->setKey(k);
 
@@ -279,7 +279,7 @@ int XSEC_EXPORT CalculateXKMSRevocationCodeIdentifierEncoding1(unsigned char * i
 	Janitor<XSECCryptoKeyHMAC> j_k(k);
 	k->setKey(keyVal, 1);
 
-	XSECCryptoHash *h = XSECPlatformUtils::g_cryptoProvider->hashHMACSHA1();
+	XSECCryptoHash *h = XSECPlatformUtils::g_cryptoProvider->hashHMACSHA();
 	Janitor<XSECCryptoHash> j_h(h);
 
 	h->setKey(k);
@@ -310,7 +310,7 @@ int XSEC_EXPORT CalculateXKMSRevocationCodeIdentifierEncoding2From1(unsigned cha
 	Janitor<XSECCryptoKeyHMAC> j_k(k);
 	k->setKey(keyVal, 1);
 
-	XSECCryptoHash *h = XSECPlatformUtils::g_cryptoProvider->hashHMACSHA1();
+	XSECCryptoHash *h = XSECPlatformUtils::g_cryptoProvider->hashHMACSHA();
 	Janitor<XSECCryptoHash> j_h(h);
 
 	h->setKey(k);
@@ -337,7 +337,7 @@ int XSEC_EXPORT CalculateXKMSKEK(unsigned char * input, int inputLen, unsigned c
 		XSECCryptoKeyHMAC * k = XSECPlatformUtils::g_cryptoProvider->keyHMAC();
 		k->setKey(shaOutput, keyLen);
 
-		XSECCryptoHash *h = XSECPlatformUtils::g_cryptoProvider->hashHMACSHA1();
+		XSECCryptoHash *h = XSECPlatformUtils::g_cryptoProvider->hashHMACSHA();
 		Janitor<XSECCryptoHash> j_h(h);
 
 		h->setKey(k);

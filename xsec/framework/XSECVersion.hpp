2d3ead3f3b19a909df9eb2a7efdfce84d4b278cd
Scott Cantor <scantor@apache.org>
2018-08-01 14:07:25 +0000

    Update version to 2.0.1.
    git-svn-id: https://svn.apache.org/repos/asf/santuario/xml-security-cpp/trunk@1837241 13f79535-47bb-0310-9956-ffa450edef68

--- xsec/framework/XSECVersion.hpp
+++ xsec/framework/XSECVersion.hpp
@@ -30,7 +30,7 @@
 
 #define XSEC_VERSION_MAJOR   2
 #define XSEC_VERSION_MEDIUM  0
-#define XSEC_VERSION_MINOR   0
+#define XSEC_VERSION_MINOR   1
 
 // --------------------------------------------------------------------------------
 //           Version Handling
